744b67a354737ed9c8b24e97894acf0c50422c1b
Oris104 <siro.covo@gmail.com>
2022-10-23 13:04:29 +0200

    Kommentation Code

--- main.py
+++ main.py
@@ -13,66 +13,57 @@ import keyboard
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+command = "mode 75,50" # Fenstergrösse einstellen
+
+os.system(command) # Fenstergrösse ausführen
+os.system('color 02') # Farbe konfigurieren
+numLines = shutil.get_terminal_size().lines # anzahl Zeilen auslesen
+numCols = shutil.get_terminal_size().columns # anzahl Kollonen auslesen
+
+Field = np.arange(numCols*numLines).reshape(numLines,numCols) #2D Array mit Zeilen und Kollonen erstellen
 
 
-
-
-
-command = "mode 75,50"
-
-os.system(command)
-os.system('color 02')
-numLines =shutil.get_terminal_size().lines
-numCols =shutil.get_terminal_size().columns
-
-Field =  np.arange(numCols*numLines).reshape(numLines,numCols)
-
-
-
-def Printer():
+def Printer(): # Gibt Spielfeld aus nachdem Array Werte mit zugehörigem Zeichen ersetzt wurden
     print(str(Field).replace(", ", "").replace("[", "").replace("]", "").replace("\n", "").replace(" ", "").replace("0",
     " ").replace("2", "A").replace("4","V").replace("6","I")+"Score: "+str(score)+"  Lives: "+str(lives))
 
 
-
-np.set_printoptions(threshold=sys.maxsize)
-
+np.set_printoptions(threshold=sys.maxsize) # Array Ausgabe einrichten
 
 
-np.place(Field,Field>=0,0)
+np.place(Field,Field>=0,0) # Füllt Array mit 0
+# Gibt Leeres Feld zum Start aus
 print(str(Field).replace(", ","").replace("[","").replace("]","").replace("\n","").replace(" ","").replace("0", " "))
-X=0
-y=0
-
-
-i=0
-enemies=[]
-projectiles=[]
-player=GameEntities.Player(49,30,Field)
-zc=0
-score =0
-lives = 5
-cd=0
-spwn =10
-while lives > 0:
-    if spwn >=20:
-        spwn =0
+X = 0 # Koordinaten Variablen setzen
+y = 0 #
+i = 0 # Zähl variable setzen
+enemies = [] # Liste für gegner objekte erstellen
+projectiles = [] # Liste für Projektile erstellen
+player = GameEntities.Player(49,30,Field) # Spieler erstellen
+enemyMov = 0 # Zählvariable gegner erstellen
+score = 0 # score Variable erstellen
+lives = 5 # Variable für Leben erstellen
+cd = 0 # Schuss Cooldown Variable erstellen
+spwn = 10 # Gegner Spawn Cooldown Variable einstellen
+while lives > 0: # Loop solange man noch Leben hat
+    if spwn >= 20: # Alle 20 Ticks
+        spwn = 0
         u = random.randint(0, numCols - 1)
-        enemies.append(GameEntities.Enemy(0, u, 4, Field))
+        enemies.append(GameEntities.Enemy(0, u, 4, Field)) # Gegner an Zufälliger Position erstellen
     else:
         spwn +=1
-    if zc == 5:
-        zc=0
+    if enemyMov == 5:
+        enemyMov=0
         for it in enemies:
-            neo = it.tick()
-            if neo ==1:
+            rück = it.tick()
+            if rück ==1:
                 score +=1
                 enemies.remove(it)
-            elif neo ==2:
+            elif rück ==2:
                 enemies.remove(it)
                 lives +=-1
     else:
-        zc += 1
+        enemyMov += 1
 
 
 
