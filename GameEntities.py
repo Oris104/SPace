481d4cea230f8753ff49d5bacbedf1018e15a107
Oris104 <siro.covo@gmail.com>
2022-10-23 14:31:43 +0200

    Code Cleanup und Kommentieren

--- GameEntities.py
+++ GameEntities.py
@@ -1,66 +1,75 @@
 import keyboard
 
-class Chara():
-    def __init__(self,x,y,val,Field):
-        self.x=x
-        self.y=y
-        self.val=val
-        self.Field = Field
-        self.createAt()
 
+class Chara():  # Grundklasse Charakter
+    def __init__(self, x, y, val, Field):  # Basis initialisierung
+        self.x = x
+        self.y = y
+        self.val = val
+        self.Field = Field
+        self.createAt()  # Schreibt eigenen wer an Position in Array
 
     def createAt(self):
-        self.Field[self.x][self.y]=self.val
-    def moveDown(self):
+        self.Field[self.x][self.y] = self.val
 
-        self.Field[self.x+1][self.y]= self.Field[self.x][self.y]
-        self.Field[self.x][self.y]=0
-        self.x+=1
+    def moveDown(self):  # Schreibt Wert ein Feld nach unten
 
+        self.Field[self.x + 1][self.y] = self.Field[self.x][self.y]
+        self.Field[self.x][self.y] = 0
+        self.x += 1
 
-    def moveUp(self):
+    def moveUp(self):  # Schreibt Wert ein Feld nach oben
         self.Field[self.x - 1][self.y] = self.Field[self.x][self.y]
         self.Field[self.x][self.y] = 0
         self.x -= 1
-    def moveRight(self):
-        if self.y+1<=73:
-            self.Field[self.x][self.y+1] = self.Field[self.x][self.y]
+
+    def moveRight(self):  # Schreibt Wert ein Feld nach rechts
+        if self.y + 1 <= 73:
+            self.Field[self.x][self.y + 1] = self.Field[self.x][self.y]
             self.Field[self.x][self.y] = 0
             self.y += 1
-    def moveLeft(self):
-        if self.y>1:
-            self.Field[self.x][self.y-1] = self.Field[self.x][self.y]
+
+    def moveLeft(self):  # Schreibt Wert ein Feld nach links
+        if self.y > 1:
+            self.Field[self.x][self.y - 1] = self.Field[self.x][self.y]
             self.Field[self.x][self.y] = 0
             self.y -= 1
-class Player(Chara):
-    def __init__(self, x, y, Field):
+
+
+class Player(Chara):  # Spielercharakter
+    def __init__(self, x, y, Field):  # Schreibt sich selbst in festes Felf
         super().__init__(x, y, 2, Field)
-        self.val =2
-    def shoot(self):
-        pass
+        self.val = 2
+
     def tick(self):
         self.createAt()
         if keyboard.is_pressed("left"):
             self.moveLeft()
         if keyboard.is_pressed("right"):
             self.moveRight()
+
+
 class Projectile(Chara):
     def suicide(self):
-        self.Field[self.x][self.y]=0
+        self.Field[self.x][self.y] = 0
         del (self)
+
     def tick(self):
-        if not self.x >0:
+        if not self.x > 0:
             self.suicide()
         else:
             self.moveUp()
+
+
 class Enemy(Chara):
     def suicide(self):
-        self.Field[self.x][self.y]=0
+        self.Field[self.x][self.y] = 0
+
     def tick(self):
         if self.x >= 49:
             self.suicide()
             return 2
-        elif not self.Field[self.x][self.y]==self.val:
+        elif not self.Field[self.x][self.y] == self.val:
             self.suicide()
             return 1
         else:
